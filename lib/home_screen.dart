133f084d0d32dab5c7e12adb367407e03f453c7a
Abu Obayed <obayed15-6250@s.diu.edu.bd>
2025-01-06 11:07:36 +0600

    ok

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,13 +1,36 @@
-import 'main.dart';
+import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  RxInt _counter=0.obs;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        title: Text('Counter App'),
+        centerTitle: false,
+      ),
+      body: Center(
+        child: Obx(() {
+          return Text('$_counter', style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.w600,
+          ),);
+        }),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _OnTapAddButton, child: Icon(Icons.add),),
     );
+  }
+
+  void _OnTapAddButton(){
+    _counter++;
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,4 @@
-// import 'package:device_preview/device_preview.dart';
-// import 'package:flutter/material.dart';
-// import 'package:medicare/config/colors.dart';
-// import 'package:medicare/ui/login_screen.dart';
-//
-// void main() {
-//   runApp(
-//     DevicePreview(
-//       enabled: true,
-//       builder: (context) => MyApp(), // Wrap your app
-//     ),
-//   );
-// }
-//
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: SigninScreen(),
-//       theme: ThemeData(
-//         brightness: Brightness.light,
-//
-//       ),
-//       darkTheme:ThemeData(
-//         brightness: Brightness.dark,
-//
-//       ),
-//       themeMode: ThemeMode.system,
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
-
 import 'home_screen.dart';
 
 void main(){
